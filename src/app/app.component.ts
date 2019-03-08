041dc13f911bbbfbf33677abd30b7be2b45edf7f
John Nnanna <nacojohn@gmail.com>
2019-03-08 14:12:53 +0100

    User's Album and Photo Viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError, Event } from '@angular/router';
+import { AppLoadingService } from './services/app-loading.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-task';
+    title = 'angular-task';
+    appLoading: boolean;
+    subscription: Subscription;
+  
+    constructor(private router: Router,
+                private appLoadingService: AppLoadingService) {
+        router.events.subscribe(
+          (routerEvent: Event) => {
+            this.checkRouterEvent(routerEvent)
+          }
+        )
+    }
+  
+    ngOnInit() {
+      this.subscription = this.appLoadingService.appIsLoading.subscribe(
+        (resp: boolean) => {
+          this.appLoading = resp;
+        }
+      )
+    }
+  
+    ngOnDestroy() {
+      this.subscription.unsubscribe();
+    }
+  
+    checkRouterEvent(routerEvent: Event): void {
+        if (routerEvent instanceof NavigationStart){
+            this.appLoadingService.appIsLoading.next(true);
+        }
+  
+        if (routerEvent instanceof NavigationEnd ||
+            routerEvent instanceof NavigationCancel ||
+            routerEvent instanceof NavigationError) {
+              this.appLoadingService.appIsLoading.next(false);
+        }
+    }
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpRequest, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { DEV_STATUS, API_URL } from './app-config';
 
 @Injectable()
 export class MakeHttpRequest {
-//   private url: string = API_URL;
-  private url: string = DEV_STATUS == "production" ? API_URL : "/api/";
+  private url: string = "https://jsonplaceholder.typicode.com/";
 
   constructor(private http: HttpClient) {  }
 
@@ -36,118 +33,15 @@ export class MakeHttpRequest {
     if (userAuthorization) {
         headers = new HttpHeaders()
                       .set("Accept","application/json")
-                    //   .append("X-Requested-With","XMLHttpRequest")
-                    //   .append("Content-Type", "text/plain")
                       .append("Authorization", "Bearer " + userAuthorization);
     } else {
-    headers = new HttpHeaders()
-                    .set("Accept","application/json")
-                    // .append("Content-Type", "text/plain")
-                    // .append("X-Requested-With","XMLHttpRequest");
+        headers = new HttpHeaders()
+                    .set("Accept","application/json");
     }
 
     return this.http.post(this.url + queryParam, postValue, {
       responseType: 'json',
       headers: headers
     })
-  }
-
-  updateRequest(queryParam: string, postValue: object, userAuthorization?) {
-    let headers: HttpHeaders;
-
-    if (userAuthorization) {
-        headers = new HttpHeaders()
-                      .set("Accept","application/json")
-                    //   .append("X-Requested-With","XMLHttpRequest")
-                    //   .append("Content-Type", "text/plain")
-                      .append("Authorization", "Bearer " + userAuthorization);
-    } else {
-    headers = new HttpHeaders()
-                    .set("Accept","application/json")
-                    // .append("Content-Type", "text/plain")
-                    // .append("X-Requested-With","XMLHttpRequest");
-    }
-
-    return this.http.patch(this.url + queryParam, postValue, {
-      responseType: 'json',
-      headers: headers
-    })
-  }
-
-  deleteRequest(queryParam: string, postValue: object, userAuthorization?) {
-    let headers: HttpHeaders;
-
-    if (userAuthorization) {
-        headers = new HttpHeaders()
-                      .set("Accept","application/json")
-                    //   .append("X-Requested-With","XMLHttpRequest")
-                    //   .append("Content-Type", "text/plain")
-                      .append("Authorization", "Bearer " + userAuthorization);
-    } else {
-    headers = new HttpHeaders()
-                    .set("Accept","application/json")
-                    // .append("Content-Type", "text/plain")
-                    // .append("X-Requested-With","XMLHttpRequest");
-    }
-
-    return this.http.delete(this.url + queryParam, {
-      responseType: 'json',
-      headers: headers
-    })
-  }
-
-  createFormData(object: Object, form?: FormData, namespace?: string): FormData {
-      const formData = form || new FormData();
-      for (let property in object) {
-          if (!object.hasOwnProperty(property) || !object[property]) {
-              continue;
-          }
-          const formKey = namespace ? `${namespace}[${property}]` : property;
-          if (object[property] instanceof Date) {
-              formData.append(formKey, object[property].toISOString());
-          } else if (typeof object[property] === 'object' && !(object[property] instanceof File)) {
-              this.createFormData(object[property], formData, formKey);
-          } else {
-              formData.append(formKey, object[property]);
-          }
-      }
-      return formData;
-  }
-
-  fileUpload(parameter: string, fileItem: File[] | File, extraData:object, userAuthorization): any {
-    let headers: HttpHeaders = new HttpHeaders()
-                  .append("Authorization", "Bearer " + userAuthorization);
-
-      let apiCreateEndPoint = `${this.url}${parameter}`;
-      let formData: FormData = new FormData();
-
-      if (Array.isArray(fileItem)) {
-        for (let i = 0; i < fileItem.length; i++) {
-            if (fileItem[i]) {
-                formData.append('image' + i, fileItem[i], fileItem[i].name);
-            }
-        }
-      } else {
-          console.log(fileItem)
-        formData.append('image', fileItem, fileItem.name);
-      }
-
-      // formData.append('fileItem', fileItem, fileItem.name);
-      if (extraData) {
-          formData = this.createFormData(extraData, formData)
-      }
-
-      const req = new HttpRequest('POST', apiCreateEndPoint, formData, {
-          reportProgress: true,
-          responseType: 'json',
-          headers: headers
-      });
-
-      return this.http.request(req);
-  }
-
-  list(): Observable<any>{
-      const listEndpoint = `${this.url}files/`
-      return this.http.get(listEndpoint)
   }
 }
